7485b572bd2674187873c8c69904a8c996c4c633
hagen <toennies@retresco.de>
2014-01-17 12:56:13 +0100

    Add a redis spout and string splitter.
    Signed-off-by: hagen <toennies@retresco.de>

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
   :aot :all
   :min-lein-version "2.0.0"
   :dependencies [[org.slf4j/slf4j-log4j12 "1.6.1"]
-                 [commons-collections/commons-collections "3.2.1"]]
+                 [commons-collections/commons-collections "3.2.1"]
+                 [com.taoensso/carmine "2.4.4"]]
 
   :profiles {:dev {:dependencies [[storm "0.8.2"]
                      [org.clojure/clojure "1.5.1"]
--- src/clj/storm/rolling_topology.clj
+++ src/clj/storm/rolling_topology.clj
@@ -1,8 +1,10 @@
 (ns storm.rolling-topology
   (:import (backtype.storm StormSubmitter LocalCluster)
-           (jvm.bolt RollingCountBolt IntermediateRankingsBolt TotalRankingsBolt)
-           (backtype.storm.testing TestWordSpout))
-  (:use [backtype.storm clojure config])
+   (jvm.bolt RollingCountBolt IntermediateRankingsBolt TotalRankingsBolt)
+   (backtype.storm.testing TestWordSpout))
+  (:use [backtype.storm clojure config]
+        [utils.redis]
+        [clojure.string])
   (:gen-class))
 
 (def TOP_N 5)
@@ -10,34 +12,57 @@
 (defspout word-spout ["word"]
   [conf context collector]
   (let [words ["storm"
-                   "development"
-                   "is"
-                   "even"
-                   "more"
-                   "enjoyable"
-                   "on"
-                   "a"
-                   "MacBook"]]
-    (spout
+   "development"
+   "is"
+   "even"
+   "more"
+   "enjoyable"
+   "on"
+   "a"
+   "MacBook"]]
+   (spout
      (nextTuple []
        (Thread/sleep 100)
        (emit-spout! collector [(rand-nth words)])         
        )
      (ack [id]
-        ))))
+      ))))
+
+; a spout reading strings from a redis list.
+
+(defspout sentence-spout ["sentence"]
+  [conf context collector]
+  (spout
+   (nextTuple []
+     (let [string (lpop-redis "mylist")]
+       (cond string
+        (emit-spout! collector [string])
+        )))
+
+   (ack [id])))
+
+(defbolt split-string ["word"] [tuple collector]
+  (let [ line   (.getString tuple 0)
+         words (split line #"\s+")]
+      (doseq [word words]
+        (emit-bolt! collector [word] :anchor tuple)))      
+  (ack! collector tuple))
 
 (defn mk-topology []
   (topology
-   {"spout" (spout-spec word-spout)}
-   {"counter" (bolt-spec {"spout" ["word"]}
-                   (RollingCountBolt. 9 3)
-                   :parallelism-hint 4)
-    "intermediateRanker" (bolt-spec {"counter" ["obj"]}
-                   (IntermediateRankingsBolt. TOP_N)
-                   :parallelism-hint 4)
-    "finalRanker" (bolt-spec {"intermediateRanker" :global}
-                   (TotalRankingsBolt. TOP_N)
-                   :parallelism-hint 1)}))
+   ;{"spout" (spout-spec word-spout)}
+   {"spout" (spout-spec sentence-spout)}
+   {"splitter" (bolt-spec {"spout" :shuffle} split-string :p 3)
+    ; oh my god java
+    "counter" (bolt-spec {"splitter" ["word"]}
+     (RollingCountBolt. 9 3)
+     :parallelism-hint 4)
+   "intermediateRanker" (bolt-spec {"counter" ["obj"]}
+     (IntermediateRankingsBolt. TOP_N)
+     :parallelism-hint 4)
+   "finalRanker" (bolt-spec {"intermediateRanker" :global}
+     (TotalRankingsBolt. TOP_N)
+     :parallelism-hint 1)}))
 
 (defn run-local! []
   (let [cluster (LocalCluster.)]
@@ -51,7 +76,7 @@
    name
    {TOPOLOGY-DEBUG true
     TOPOLOGY-WORKERS 3}
-   (mk-topology)))
+    (mk-topology)))
 
 (defn -main
   ([]
